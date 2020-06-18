dd7332fc062997596d3c501879e2879b43ca2c00
Saharsh <saharshg10@gmail.com>
2020-06-18 22:52:28 +0530

    add build

---
+++ build/precache-manifest.5c4c3b4ea304f8e2490ccd50f8fe9c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f870ee5cbb6cd36ee3f79f7f6e64b5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ed7032b9d3c638fb8097",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ce9a0f7349ae0edd7e1d",
+    "url": "/portfolio/static/js/2.f4397d7d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.f4397d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7032b9d3c638fb8097",
+    "url": "/portfolio/static/js/main.22e8ca6f.chunk.js"
+  },
+  {
+    "revision": "f6fd1d67c131d6335806",
+    "url": "/portfolio/static/js/runtime-main.45c4eb3f.js"
+  }
+]);
