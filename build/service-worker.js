c31e84b1c5cf1840ca9aa07ff02634bdff924b3c
Saharsh <saharshg10@gmail.com>
2020-09-10 01:36:51 +0530

    update meta image

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79daec8648e13ca3927cbf957dc230f9.js"
+  "/precache-manifest.2083477f485e83d7b594f68d85fcd936.js"
 );
 
 self.addEventListener('message', (event) => {
