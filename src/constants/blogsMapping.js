a3b4e1153e262c5a38680f0f76c517139e3ef98f
Saharsh <saharshg10@gmail.com>
2022-05-19 01:56:42 +0530

    Re order blogs

--- src/constants/blogsMapping.js
+++ src/constants/blogsMapping.js
@@ -4,19 +4,14 @@ import NegativeZero from '../screens/Blogs/negativeZero';
 import Markdown from '../screens/Blogs/Markdown';
 
 export const blogsMapping = {
-  closures: {
-    title: 'Closures in JavaScript',
-    createdAt: 'July 12, 2020',
-    modifiedAt: 'September 16, 2020',
-    slug: 'Briefly understanding closure and the lexical environment in javascript.',
-    hashtags: ['closures', 'scoping', 'javascript'],
-    component: <Closure />,
-    url: 'https://saharshgoyal.com/#/blogs/closures',
-    featureImage: {
-      src: 'https://images.unsplash.com/photo-1450562624248-869a3ec195f1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-      url: 'https://unsplash.com/@cblack09?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText',
-      credit: 'Cody Black',
-    },
+  accessibilityfornoninteractiveelements: {
+    title: 'Accessibility for non-interactive HTML elements in React',
+    createdAt: 'May 18, 2022',
+    modifiedAt: 'May 18, 2022',
+    slug: 'Making non-interactive HTML elements accessible',
+    hashtags: ['accessibility', 'html', 'non-interactive elements'],
+    component: <Markdown fileName='NonInteractiveElementsAccessibility.md' />,
+    url: 'https://saharshgoyal.com/#/blogs/accessibilityfornoninteractiveelements',
   },
   negativezero: {
     title: 'Negative Zero in JavaScript',
@@ -32,14 +27,19 @@ export const blogsMapping = {
       credit: 'Melanie Dretvic',
     },
   },
-  accessibilityfornoninteractiveelements: {
-    title: 'Accessibility for non-interactive HTML elements in React',
-    createdAt: 'May 18, 2022',
-    modifiedAt: 'May 18, 2022',
-    slug: 'Making non-interactive HTML elements accessible',
-    hashtags: ['accessibility', 'html', 'non-interactive elements'],
-    component: <Markdown fileName='NonInteractiveElementsAccessibility.md' />,
-    url: 'https://saharshgoyal.com/#/blogs/accessibilityfornoninteractiveelements',
+  closures: {
+    title: 'Closures in JavaScript',
+    createdAt: 'July 12, 2020',
+    modifiedAt: 'September 16, 2020',
+    slug: 'Briefly understanding closure and the lexical environment in javascript.',
+    hashtags: ['closures', 'scoping', 'javascript'],
+    component: <Closure />,
+    url: 'https://saharshgoyal.com/#/blogs/closures',
+    featureImage: {
+      src: 'https://images.unsplash.com/photo-1450562624248-869a3ec195f1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+      url: 'https://unsplash.com/@cblack09?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText',
+      credit: 'Cody Black',
+    },
   },
   // usestate: {
   //   title: 'Managing local state with React.useState()',
